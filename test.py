2f4877c7a83cfcc114ad70a0fa6deadc8926562b
Mathieu Hannoun <mathieu95480@gmail.com>
2019-10-21 10:42:37 +0200

    :sparkles: Added video capture

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('example2.jpg', flags=cv2.IMREAD_COLOR)
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cap = cv2.VideoCapture(0)
 
-print('Image shape :', img.shape)
+while (True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.IMREAD_COLOR)
+    print('Current FPS : ', cap.get(cv2.CAP_PROP_FPS))
+    # Display the resulting frame
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
